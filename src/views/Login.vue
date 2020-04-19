434daca48288288ef8cc19f3b3fe5deccffda111
xuxiang <xuxiang2815@163.com>
2020-04-19 21:49:58 +0800

    第一次提交

---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>欢迎登陆login</h1>
+  </div>
+</template>
+
+<script>
+
+// export default {
+//   name: "Home",
+//   components: {
+//     HelloWorld
+//   }
+// };
+</script>
